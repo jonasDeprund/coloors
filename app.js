ffbc5aaf84cb931c906f130512bba0866f9d3d0c
Jonas Deprund <jonasdeprund@gmail.com>
2021-03-29 19:13:00 +0200

    sliders done

--- app.js
+++ app.js
@@ -75,6 +75,14 @@ function hslControls(e) {
   const hue = sliders[0]
   const brightness = sliders[1]
   const saturation = sliders[2]
+
+  const bgColor = colorDivs[index].querySelector('h2').innerText
+  let color = chroma(bgColor)
+    .set('hsl.s', saturation.value)
+    .set('hsl.l', brightness.value)
+    .set('hsl.h', hue.value)
+
+  colorDivs[index].style.backgroundColor = color
 }
 
 randomColors()
