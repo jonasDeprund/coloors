323f350a91dc63453702528a8619c8205c578c61
Jonas Deprund <jonasdeprund@gmail.com>
2021-03-31 16:54:47 +0200

    fixed bug done

--- app.js
+++ app.js
@@ -29,9 +29,11 @@ function generateHex() {
 }
 
 function randomColors() {
+  initialColors = []
   colorDivs.forEach((div, index) => {
     const hexText = div.children[0]
     const randomColor = generateHex()
+    initialColors.push(chroma(randomColor).hex())
     div.style.backgroundColor = randomColor
     hexText.innerHTML = randomColor
     checkTextContrast(randomColor, hexText)
@@ -82,7 +84,7 @@ function hslControls(e) {
   const brightness = sliders[1]
   const saturation = sliders[2]
 
-  const bgColor = colorDivs[index].querySelector('h2').innerText
+  const bgColor = initialColors[index]
   let color = chroma(bgColor)
     .set('hsl.s', saturation.value)
     .set('hsl.l', brightness.value)
